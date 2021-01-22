f5bb5a916d0d18c1de57161912164ee62961612b
Developer Mazharul <mazharulweb@gmail.com>
2021-01-22 15:07:15 +0600

    second commit

---
+++ assignment.js
@@ -0,0 +1,14 @@
+// GitHub
+
+
+// first concept
+function kilometerToMeter(kilometer) {
+    var meter = 0;
+    if (kilometer < 0) {}
+    else {
+        meter = kilometer + 10;
+    }
+    return meter;
+}
+var result = kilometerToMeter(2);
+console.log(result);
