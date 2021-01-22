d6cc0ad966c240c714a584037e9bd4ae3980ebaf
Developer Mazharul <mazharulweb@gmail.com>
2021-01-22 21:22:26 +0600

    problem commit

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
-// GitHub
+// https://github.com/Developer-Mazharul/javascript-assignment
 
 
-// first concept
+// kilometerToMeter Start
 function kilometerToMeter(kilometer) {
     var meter = 0;
     if (kilometer < 0) {}
@@ -12,3 +12,46 @@ function kilometerToMeter(kilometer) {
 }
 var result = kilometerToMeter(2);
 console.log(result);
+// KilometerToMeter End
+
+
+// budgetCalculator Start
+function budgetCalculator(watch,phone,laptop) {
+    var allprice = 0;
+    if (watch < 0) {}
+    else if (phone < 0) {}
+    else if (laptop < 0) {}
+    else {
+        var watch = watch * 50;
+        var phone = phone * 100;
+        var laptop = laptop * 500;
+        allprice = watch + phone + laptop;
+    }
+    return allprice;
+}
+var result = budgetCalculator(30,20,10);
+console.log(result);
+// budgetCalculator End
+
+
+// hotelCost Start
+
+//hotelCost End
+
+
+// megaFriend Start
+var names = ['Bashar', 'Habib', 'Somrat', 'Sabbir', 'Naim'];
+function megaFrind(friendsName) {
+    var biggestName = friendsName [0];
+    for (var i = 0; i < friendsName.length; i++) {
+        var element = friendsName [i];
+        var highLength = element.length;
+        if (biggestName.length < highLength) {
+            biggestName = element
+        }
+    }
+    return biggestName;
+}
+var result = megaFrind(names);
+console.log(result);
+// megaFriend End
